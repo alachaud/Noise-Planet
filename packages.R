18563dab67e356b7a0ea808f20dffd4337df0287
Amélie Lachaud <amelielachaud@orange.fr>
2019-01-24 17:10:37 +0100

    Ajout packages

---
+++ packages.R
@@ -0,0 +1,37 @@
+# :::::::::::::::::::: GROUPE A ::::::::::::::::::::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::
+# ::::::::::::::::: Antoine BELLOIS ::::::::::::::::
+# :::::::::::::::::: Hélène FOULON :::::::::::::::::
+# :::::::::::::::::: Amélie LACHAUD ::::::::::::::::
+# ::::::::::::::::: Delphine LE BAIL :::::::::::::::
+# ::::::::::::::::: Hugo MICCINILLI ::::::::::::::::
+# ::::::::::::::: Hugo MORANGE-GAPIHAN :::::::::::::
+# ::::::::::::::::::: Fiona TUFFIN :::::::::::::::::
+# ::::::::::::::::::::::::::::::::::::::::::::::::::
+
+install.packages("sf")
+install.packages("dplyr")
+install.packages("raster")
+install.packages("highcharter")
+install.packages("readr")
+install.packages("rgdal")
+install.packages("geojsonio") # geojson_read(")
+install.packages("sp")
+install.packages("maps")
+install.packages("ggmap") # cartes avec ggplot2
+install.packages("maptools")
+install.packages("ggplot2") # ggplot(")
+install.packages("tm") # text mining
+install.packages("RColorBrewer")
+install.packages("knitr")
+install.packages("htmltools")
+install.packages("rmarkdown")
+install.packages("flexdashboard")
+install.packages("shiny")
+install.packages("spData")#import
+install.packages("tidyverse")
+install.packages("plotly")
+install.packages("wordcloud") # nuage de mots
+install.packages("leaflet")
+install.packages("rgdal")
+install.packages("forecast")
